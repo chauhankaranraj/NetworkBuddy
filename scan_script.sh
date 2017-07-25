aaed03e36f3ad942173a99808447f7dea307e9fa
Karanraj Chauhan <chauhank@bu.edu>
2017-07-24 22:45:17 -0400

    initial commit to remote

--- scan_script.sh
+++ scan_script.sh
@@ -1,2 +1,2 @@
-cat /Users/karanraj/Documents/MIThackathon/scan_results.txt >> /Users/karanraj/Documents/MIThackathon/log.txt
-sudo nmap -sn 192.168.0.1/24 > /Users/karanraj/Documents/MIThackathon/scan_results.txt
+cat /home/pi/Documents/MIThackathon/scan_results.txt >> /home/pi/Documents/MIThackathon/log.txt
+sudo nmap -sn 192.168.0.1/24 > /home/pi/Documents/MIThackathon/scan_results.txt
