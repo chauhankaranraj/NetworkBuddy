5ed784dc913a5676a9f910fcbd81508a3d993200
karan2019 <chauhank@bu.edu>
2017-08-27 17:14:48 -0400

    commented and cleaned up MacScanner.py

--- MacScanner.py
+++ MacScanner.py
@@ -1,53 +1,23 @@
-# ver 1.0
 import os
 import re
-# import subprocess
 
 def getMacAddresses():
-    os.system('chmod +x scan_script.sh')
-    os.system('sudo ./scan_script.sh')
+    """
+    Runs the script that scans the local network and gets the mac addresses of connected devices
+    
+    :return: macAddresses: list of mac addresses of all devices currently connected to network
+    """
+    
+    os.system('chmod +x scan_script.sh')    # give required permissions
+    os.system('sudo ./scan_script.sh')      # run the script which scans the network and saves results in scan_results.txt
 
     macAddresses = []
+    
+    # open the results file and see if we have any mac addresses; if we do then add them to the list
     with open('scan_results.txt', 'r') as outputFile:
         for line in outputFile:
             if (line[0:3] == "MAC"):
-                # print(line.split()[2]+" "+ re.search(r'\((.*?)\)',line).group(1))
+                # re to strip off irrelevant stuff and get the actual mac address
                 macAddresses.append(line.split()[2]+" "+ re.search(r'\((.*?)\)',line).group(1))
 
     return macAddresses
-
-# timestamp,
-
-# print(getMacAddresses())
-
-# os.getenv("SUDO_USER")
-
-# os.system('./scan_script.sh')
-
-# result = subprocess.run(['nmap', '-sn', '192.168.0.1/24'], stdout=subprocess.PIPE)
-# print(result.stdout.decode('utf-8'))
-
-
-# sudo
-
-
-# result = subprocess.run(['sudo','nmap', '-sn', '192.168.0.1/24'], stdout=subprocess.PIPE)
-# # print(result.stdout.decode('utf-8'))
-# output = result.stdout.decode('utf-8').split()
-#
-# # i=0
-# # for item in result.stdout.decode('utf-8').split():
-# #     print("item is " + str(i) +" " +item)
-# #     i+=1
-# ipAddresses = []
-# for index in range(4, len(output), 10):
-#     ipAddresses.append(output[index])
-#
-# macAddresses = []
-# for index in range(len(output)):
-#     macAddresses.append(output[index])
-
-
-
-
-# sudo nmap -sn 192.168.0.1/24
